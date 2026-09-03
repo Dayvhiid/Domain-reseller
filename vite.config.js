9d49258d71879ac0673c3297c9b5d3c0df638983
Dayvhiid <daviddada360@gmail.com>
2026-09-03 15:16:40 +0100

    vite permission issues

--- vite.config.js
+++ vite.config.js
@@ -28,7 +28,7 @@ export default defineConfig({
   },
   
   build: {
-    outDir: '../domain-reseller-backend/public',
+    outDir: 'dist',
     emptyOutDir: true,
     rollupOptions: {
       input: {
